ec3380d56c80d532c2f9b18d1e2ec68e80468b78
Roman Zhitnik <zhitnikr@gmail.com>
2021-07-14 11:15:00 -0500

    Nearly done with react - just need to switch key and value

--- react-client/components/App.jsx
+++ react-client/components/App.jsx
@@ -7,9 +7,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: 'display this',
+      quote: 'Default Quote',
+      field: '',
     }
     this.handleGet = this.handleGet.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   };
 
   componentDidMount() {
@@ -26,9 +29,33 @@ class App extends React.Component {
 
   handleGet() {
     console.log('test');
-    // event.preventDefault();
+    event.preventDefault();
     // console.log('you hit get!'); WORKS
     axios.get('http://localhost:3000/quotes')
+        .then((response) => {
+          this.setState({quote: response.data})
+        })
+        // console.log(response.data)
+        .catch((error) => {
+            this.setState({ quote: 'No quotes' });
+            console.error('There was an error!', error);
+        });
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    console.log('a change!');
+    // console.log(event.target.value)
+    this.setState({field: event.target.value});
+    // console.log(this.state)
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('you hit submit! ');
+    axios.post('http://localhost:3000/quotes', {
+      field: this.state.field
+    })
         .then((response) => {
           this.setState({quote: response.data})
         })
@@ -45,7 +72,8 @@ class App extends React.Component {
       <div>
         <h3>RandomQuotes!</h3>
         <div>{'this.state.quote = ' + this.state.quote}</div>
-        <button onClick={this.handleGet}>Get</button>
+        <input value={this.state.value} onChange={this.handleChange}/>
+        <button onClick={this.handleGet}>Get</button><button onClick={this.handleSubmit}>Submit</button>
         {/* <div>
           <CurrentQuote quote={this.state.quote}/>
         </div>

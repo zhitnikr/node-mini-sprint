6e977a77b7a046cf344d2a6b57655e0a5addee6d
Roman Zhitnik <zhitnikr@gmail.com>
2021-07-14 08:39:47 -0500

    Missing 1 jsx

---
+++ react-client/components/currentQuote.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// const CurrentQuote = (props) => {
+//   return (
+//     <div>
+//       <div>{'What is props even? props = ' + JSON.stringify(props)}</div>
+//       <div>{'props.quote = ' + props.quote}</div>
+//     </div>
+//   )
+// }2
+
+class CurrentQuote extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        {/* <div>{'What is props even? props = ' + JSON.stringify(this.props)}</div> */}
+        <div>{'this.props.quote = ' + this.props.quote}</div>
+      </div>
+    )
+  }
+}
+export default CurrentQuote;
